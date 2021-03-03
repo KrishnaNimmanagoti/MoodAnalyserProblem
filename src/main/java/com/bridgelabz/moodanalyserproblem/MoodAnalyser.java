9dae6cb5c4ae67562f041781737992c6bbba4d28
KrishnaNimmanagoti <krishna32226@gmail.com>
2021-03-03 19:20:57 +0530

    Initial commit

---
+++ src/main/java/com/bridgelabz/moodanalyserproblem/MoodAnalyser.java
@@ -0,0 +1,20 @@
+package com.bridgelabz.moodanalyserproblem;
+
+public class MoodAnalyser {
+
+    public String analyseMood(String message){
+
+        if (message.contains("sad")){
+
+            return "SAD";
+
+        }
+        else {
+
+            return "HAPPY";
+
+        }
+
+    }
+
+}
